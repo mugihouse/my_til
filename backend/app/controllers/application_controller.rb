f38f6b29a4e9b8c2fbf91dfb1fc21fb5497076fb
mugihouse <mugi.stnk100122@gmail.com>
2024-03-08 21:51:17 +0900

    Add: APIモードでのエラー対策

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
+        include DeviseHackFakeSession
+        before_action :configure_permitted_parameters, if: :devise_controller?
+        before_action do
+                I18n.locale = :ja
+        end
+
+        # デフォルトはemailとpasswordのみ変更可能
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        end
 end

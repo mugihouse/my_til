e7e2e39c5088ea6bfa8f94d70ddc52c999cc072c
mugihouse <mugi.stnk100122@gmail.com>
2024-03-08 21:44:42 +0900

    Add: ルーティング設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :home, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end

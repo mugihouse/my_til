5c0b03865e2020528e3d0b81ddd9bef95d06f49f
mugihouse <mugi.stnk100122@gmail.com>
2024-03-12 00:44:57 +0900

    Add: 認証機能のラッパー関数追加

---
+++ frontend/lib/auth.js
@@ -0,0 +1,26 @@
+// 認証機能の制御
+export const withAuthServerSideProps = (url) => {
+  return async (context) => {
+    const { req, res } = context;
+
+    const response = await fetch(`${process.env.API_ORIGIN}/${url}`, {
+      headers: {
+        "Content-Type": "application/json",
+        uid: req.cookies["uid"],
+        client: req.cookies["client"],
+        "access-token": req.cookies["access-token"],
+      },
+    });
+    if (!response.ok && response.status === 401) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+    // TODO: 他にも500エラーを考慮した分岐も必要
+    const props = await response.json();
+    return { props };
+  };
+};

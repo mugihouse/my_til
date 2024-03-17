9e3a341cd0e6d034f9bfc25eeb647ca069d0ab3f
mugihouse <mugi.stnk100122@gmail.com>
2024-03-18 00:44:53 +0900

    Change: migrate再実行

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_03_08_065754) do
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false

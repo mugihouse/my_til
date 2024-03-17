91006140cfe45db58356f45b9497136d2e46bf5e
mugihouse <mugi.stnk100122@gmail.com>
2024-03-17 20:21:26 +0900

    Add: CssBaseline追加

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,6 +4,7 @@ import PageLayout from "@/components/page-layout";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import { CssBaseline } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider theme={theme}>
+          <CssBaseline />
           <AppRouterCacheProvider>
-            <PageLayout >{children}</PageLayout>
+            <PageLayout>{children}</PageLayout>
           </AppRouterCacheProvider>
         </ThemeProvider>
       </body>

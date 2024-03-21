a037cf5e0d31c2d5767691b22341d033bf4e66b6
mugihouse <mugi.stnk100122@gmail.com>
2024-03-21 12:31:03 +0900

    Add: API側で記事情報を渡すアクション追加

--- backend/app/controllers/api/v1/articles_controller.rb
+++ backend/app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::ArticlesController < ApplicationController
   before_action :authenticate_api_v1_user!
 
+  def index
+    articles = Article.all
+    articles_hash = build_articles_array(articles)
+    render json: articles_hash, status: :ok
+  end
+
   def create
     article = current_api_v1_user.articles.new(article_params)
     article.publish_day = Time.zone.now
@@ -12,9 +18,31 @@ class Api::V1::ArticlesController < ApplicationController
     end
   end
 
+  def show
+    article = Article.find(params[:id])
+    if article
+      article_hash = {
+        id: article.id,
+        title: article.title,
+        content: article.content,
+      }
+      render json: article_hash, status: :ok
+    end
+  end
+
   private
 
   def article_params
     params.permit(:title, :content)
+  end
+
+  def build_articles_array(articles)
+    articles.map do |article|
+      {
+        id: article.id,
+        title: article.title,
+        content: article.content,
+      }
+    end
   end
 end

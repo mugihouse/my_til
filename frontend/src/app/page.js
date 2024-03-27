b857e246f23b813813c5838cd8271e210aa8fa18
mugihouse <mugi.stnk100122@gmail.com>
2024-03-27 12:52:17 +0900

    Add: トップページへ記事のタイムラインを表示

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,56 @@
-export default function Home() {
-  return <div>トップページ</div>;
-}
+"use client";
+
+import ArticleCard from "@/components/article-card";
+import { Box, Container } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+const TopPage = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}` + "/api/v1/articles",
+          {
+            headers: {
+              uid: Cookies.get("uid"),
+              client: Cookies.get("client"),
+              "access-token": Cookies.get("access-token"),
+            },
+          }
+        );
+        setArticles(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Container align="center">
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 700,
+          display: "grid",
+          mt: 3,
+        }}
+      >
+        {articles.map((item) => (
+          <ArticleCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            name={item.name}
+          />
+        ))}
+      </Box>
+    </Container>
+  );
+};
+
+export default TopPage;

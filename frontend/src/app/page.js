9481222f2c55fea5c689bc6e5dd89ec7027d61d3
mugihouse <mugi.stnk100122@gmail.com>
2024-04-03 22:36:16 +0900

    Add: トップページにpaginationを追加

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,25 @@
 "use client";
 
+import PaginationList from "@/components/pagination-list";
 import ArticleCard from "@/components/article-card";
 import { Box, Container } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
-const TopPage = () => {
+const TopPage = (context) => {
   const [articles, setArticles] = useState([]);
+  const [pagination, setPagination] = useState([]);
 
   useEffect(() => {
+    // 指定がない時は1ページ目から
+    const paged = context.searchParams.paged ?? 1;
     const fetchData = async () => {
       try {
         const res = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}` + "/api/v1/articles",
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}` +
+            "/api/v1/articles?paged=" +
+            `${paged}`,
           {
             headers: {
               uid: Cookies.get("uid"),
@@ -22,34 +28,52 @@ const TopPage = () => {
             },
           }
         );
-        setArticles(res.data);
+        setArticles(res.data.articles);
+        setPagination(res.data.pagination[0]);
+        // console.log(res.data.pagination[0]);
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
   }, []);
-
   return (
-    <Container align="center">
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: 700,
-          display: "grid",
-          mt: 3,
-        }}
-      >
-        {articles.map((item) => (
-          <ArticleCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            name={item.name}
-          />
-        ))}
-      </Box>
-    </Container>
+    <>
+      <Container align="center">
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: 700,
+            display: "grid",
+            mt: 3,
+          }}
+        >
+          {articles.map((item) => (
+            <ArticleCard
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              name={item.name}
+            />
+          ))}
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            mt: 3,
+          }}
+        >
+          <Box m="auto">
+            <PaginationList
+              pagination={pagination}
+              setArticles={setArticles}
+              setPagination={setPagination}
+            />
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 };
 
